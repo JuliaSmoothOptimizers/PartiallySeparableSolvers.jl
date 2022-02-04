288a08ae6400454803baf041b6778ebc4a89c402
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-02-04 12:46:50 +0100

    replace occurences of main by master

--- docs/make.jl
+++ docs/make.jl
@@ -14,7 +14,7 @@ makedocs(
   pages = Any["Home" => "index.md", "Tutorial" => "tutorial.md", "Reference" => "reference.md"],
 )
 
-deploydocs(repo = "github.com/paraynaud/PartiallySeparableSolvers.jl.git", devbranch = "main")
+deploydocs(repo = "github.com/paraynaud/PartiallySeparableSolvers.jl.git", devbranch = "master")
 
 
 # using DocumenterTools
