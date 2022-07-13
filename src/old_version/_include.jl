3991d95100ecc311c77421d3a6ac7e0d71d88f00
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-07-13 17:06:14 -0300

    remove uneecessary files

--- src/old_version/_include.jl
+++
@@ -1,4 +0,0 @@
-include("quasi_newton.jl") #définie les mises à jour BFGS/SR1 ainsi que leurs versions par morceaux
-include("PartitionnedSolvers.jl") #définie les solvers PSR1, PBFGS
-include("partitionned_NLPModel.jl")
-include("impl_Tr_Cg_Ab.jl") # Définie les solvers LSR1 et LBFGS
--- test/new_version/tr_cg_part_data.jl
+++ test/new_version/tr_cg_part_data.jl
@@ -1,47 +1,14 @@
-@testset "PBFGS-PLBFGS Rosenbrock" begin
-  n = 10
-  ros_mnlp = create_Rosenbrock_MathOptModel(n)
-  ros_adnlp = Rosenbrock_ADNLPModel(n)
-
-  ges_mnlp_pbfgs = PBFGS2(ros_mnlp)
-  ges_adnlp_pbfgs = PBFGS2(ros_adnlp)
-
-  ges_mnlp_plbfgs = PLBFGS(ros_mnlp)
-  ges_adnlp_plbfgs = PLBFGS(ros_adnlp)
-
-  @test ges_mnlp_pbfgs.status == :first_order
-  @test ges_adnlp_pbfgs.status == :first_order
-  @test ges_mnlp_plbfgs.status == :first_order
-  @test ges_adnlp_plbfgs.status == :first_order
-
-  @test isapprox(norm(ges_mnlp_pbfgs.solution - ones(n), 2), 0, atol = 1e-2)
-  @test isapprox(norm(ges_adnlp_pbfgs.solution - ones(n), 2), 0, atol = 1e-2)
-  @test isapprox(norm(ges_mnlp_plbfgs.solution - ones(n), 2), 0, atol = 1e-2)
-  @test isapprox(norm(ges_adnlp_plbfgs.solution - ones(n), 2), 0, atol = 1e-2)
-end
-
-@testset "PBFGS-PLBFGS Rosenbrock" begin
-  n = 10
-  nlp = MathOptNLPModel(OptimizationProblems.arwhead(n), name = "arwhead " * string(n))
-
-  ges_pbfgs = PBFGS2(nlp)
-  ges_plbfgs = PLBFGS(nlp)
-
-  @test ges_pbfgs.status == :first_order
-  @test ges_plbfgs.status == :first_order
-end
-
 @testset "PUS Rosenbrock" begin
   n = 10
   nlp = MathOptNLPModel(OptimizationProblems.arwhead(n), name = "arwhead " * string(n))
 
-  ges_plbfgs = PUS(nlp; name = :plbfgs)
-  ges_plbfgs_damped = PUS(nlp; name = :plbfgs, damped = true)
-  ges_plsr1 = PUS(nlp; name = :plsr1)
-  ges_plse = PUS(nlp; name = :plse)
-  ges_psr1 = PUS(nlp; name = :psr1)
-  ges_pse = PUS(nlp; name = :pse)
-  ges_pbfgs = PUS(nlp; name = :pbfgs)
+  ges_plbfgs = PUS(nlp; name = :plbfgs, verbose=false)
+  ges_plbfgs_damped = PUS(nlp; name = :plbfgs, damped = true, verbose=false)
+  ges_plsr1 = PUS(nlp; name = :plsr1, verbose=false)
+  ges_plse = PUS(nlp; name = :plse, verbose=false)
+  ges_psr1 = PUS(nlp; name = :psr1, verbose=false)
+  ges_pse = PUS(nlp; name = :pse, verbose=false)
+  ges_pbfgs = PUS(nlp; name = :pbfgs, verbose=false)
 
   @test ges_plbfgs.status == :first_order
   @test ges_plbfgs_damped.status == :first_order
