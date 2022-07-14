3eb063a33be02e54cf673a55d61e8fc826ef952e
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-07-14 11:44:18 -0300

    change test subrepository

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,8 @@ using JuMP, MathOptInterface
 using ADNLPModels, NLPModels, NLPModelsJuMP
 using JSOSolvers, ModelingToolkit
 
-
-include("new_version/_include.jl")
+include("models.jl")
+include("pss.jl")
+include("partitioned_methods.jl")
+include("limited_memory.jl")
+include("tr_cg_part_data.jl")
