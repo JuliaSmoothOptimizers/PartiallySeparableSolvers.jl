32b15dbd2851a89d18c137b5ade1663d037504a6
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-07-14 12:59:16 -0300

    change linkds + delete unrequired dependencies + normalize Project.toml

--- src/partitioned_methods.jl
+++ src/partitioned_methods.jl
@@ -7,6 +7,12 @@ using ..Mod_TR_CG_part_data
 export get_expr_tree
 export PUS
 
+"""
+    expr_tree, n, x0 = get_expr_tree(nlp::MathOptNLPModel; x0::Vector{T} = copy(nlp.meta.x0), kwargs...) where {T <: Number}
+    expr_tree, n, x0 = get_expr_tree(adnlp::ADNLPModel; x0::Vector{T} = copy(adnlp.meta.x0), kwargs...) where {T <: Number}
+
+Return the `expr_tree`, the size `n` and the intial point `x0` from either a `MathOptNLPModel` or a `ADNLPModel`.
+"""
 function get_expr_tree(
   nlp::MathOptNLPModel;
   x0::Vector{T} = copy(nlp.meta.x0),
@@ -16,9 +22,9 @@ function get_expr_tree(
   evaluator = JuMP.NLPEvaluator(model)
   MathOptInterface.initialize(evaluator, [:ExprGraph])
   obj_Expr = MathOptInterface.objective_expr(evaluator)::Expr
-  ex = ExpressionTreeForge.transform_to_expr_tree(obj_Expr)::ExpressionTreeForge.Type_expr_tree
+  expr_tree = ExpressionTreeForge.transform_to_expr_tree(obj_Expr)::ExpressionTreeForge.Type_expr_tree
   n = nlp.meta.nvar
-  return ex, n, x0
+  return expr_tree, n, x0
 end
 
 function get_expr_tree(
@@ -29,11 +35,24 @@ function get_expr_tree(
   n = adnlp.meta.nvar
   ModelingToolkit.@variables x[1:n]
   fun = adnlp.f(x)
-  ex = ExpressionTreeForge.transform_to_expr_tree(fun)::ExpressionTreeForge.Type_expr_tree
-  return ex, n, x0
+  expr_tree = ExpressionTreeForge.transform_to_expr_tree(fun)::ExpressionTreeForge.Type_expr_tree
+  return expr_tree, n, x0
 end
 
-function PUS(nlp::N; name = :plse, kwargs...) where {N <: AbstractNLPModel}
+"""
+    ges = PUS(nlp::AbstractNLPModel; name = :plse, kwargs...)
+
+`PUS` (partitioned update solver) return a `ges::GenericExecutionStats` from the execution of a partitioned quasi-Newton trust-region method.
+It performs a:
+
+* PBFGS method with `name=:pbfgs`;
+* PSR1 method with `name=:psr1`;
+* PSE method with `name=:pse`;
+* PLBFGS method with `name=:plbfgs`;
+* PLSR1 method with `name=:plsr1`;
+* PLSE method with `name=:plse`, by default.
+"""
+function PUS(nlp::AbstractNLPModel; name = :plse, kwargs...)
   (ex, n, x0) = get_expr_tree(nlp)
   part_data_pqn = build_PartitionedDataTRPQN(ex, n; name = name, x0 = x0)
   ges = generic_algorithm_wrapper(nlp, part_data_pqn; kwargs...)
