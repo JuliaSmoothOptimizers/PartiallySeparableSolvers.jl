0e660cc9d5ba78abb1abdc4554df1ac8e53e4e2d
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-07-18 10:09:17 -0300

    deletion limited-memory QN methods

--- src/PartiallySeparableSolvers.jl
+++ src/PartiallySeparableSolvers.jl
@@ -9,9 +9,7 @@ using ModelingToolkit
 using SolverCore
 
 export PUS # Partitioned update solver
-export my_LBFGS, my_LSR1
 
-include("tr_cg_lo_nlp.jl")
 include("tr_cg_part_data.jl")
 include("partitioned_methods.jl")
 
