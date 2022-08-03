bbc69290860188c4ff8e49d4f7b8d88e2460ddf8
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-08-03 14:43:20 -0300

    change f to NLPModel.obj + integrate get_expression_tree + intergrate new partitioned LinerOperator

--- test/pss.jl
+++ test/pss.jl
@@ -3,7 +3,9 @@
 
   nlp = arwhead(; n)
 
-  (ex, n, x0) = get_expr_tree(nlp)
+  ex = get_expression_tree(nlp)
+  n = nlp.meta.nvar
+  x0 = nlp.meta.x0
   part_data_pbfgs = build_PartitionedDataTRPQN(ex, n; x0 = x0)
 
   pbfgs_index_element_tree = part_data_pbfgs.index_element_tree
