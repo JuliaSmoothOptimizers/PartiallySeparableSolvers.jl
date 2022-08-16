5f1b743959ea067d5ddc3823473a7bd397f658dc
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-08-16 16:06:45 -0300

    add tests for PartitionedData

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,13 @@
 using PartiallySeparableSolvers
 
 using Test, LinearAlgebra
-using ExpressionTreeForge, PartiallySeparableNLPModels
+using ExpressionTreeForge, PartitionedStructures
 using ADNLPModels, NLPModels, NLPModelsJuMP
 using OptimizationProblems, OptimizationProblems.ADNLPProblems, OptimizationProblems.PureJuMP
 
 using PartiallySeparableSolvers.ModPartitionedMethods
 
+include("pqn.jl")
 include("pss.jl")
 include("partitioned_methods.jl")
 include("tr_cg_part_data.jl")
